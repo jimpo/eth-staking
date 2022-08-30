afedb9606b88d6fa2776bc543d6c326ce8cdada3
Jim Posen <jim.posen@gmail.com>
2022-08-30 10:37:18 -0400

    Switch to docker compose plugin
    Instead of legacy docker-compose standalone binary.

--- public-node/install.sh
+++ public-node/install.sh
@@ -35,23 +35,7 @@ if [[ $is_update -ne 1 ]] ; then
     $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 
     apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io
-fi
-
-# Install Docker Compose
-docker_compose_sha256=f3f10cf3dbb8107e9ba2ea5f23c1d2159ff7321d16f0a23051d68d8e2547b323
-docker_compose_path=/usr/local/bin/docker-compose
-if [[ $is_update -eq 1 && \
-      -x $docker_compose_path && \
-      $(sha256sum $docker_compose_path | cut -d ' ' -f 1) == $docker_compose_sha256 ]] ; then
-    echo "docker-compose is already latest version"
-else
-    echo "$docker_compose_sha256 docker-compose" > docker-compose.sha256
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
-    sha256sum -c docker-compose.sha256
-    chmod +x docker-compose
-    mv docker-compose /usr/local/bin
-    rm docker-compose.sha256
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 fi
 
 # Install or update docker-services systemd unit
@@ -77,7 +61,7 @@ mkdir /services
 
 # Initialize Docker services
 cp -r docker-compose.yml images authorized_keys validator-pubkeys.txt /services
-docker-compose --project-directory /services build --pull
+docker compose --project-directory /services build --pull
 
 # Configure Docker daemon
 # If docker-daemon.json changed, restart Docker service
